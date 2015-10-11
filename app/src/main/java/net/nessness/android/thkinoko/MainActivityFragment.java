2f2e4a40ec58d3d909adb43a3282196f0729c407
Yutaro Iino <ytrino@gmail.com>
2015-10-11 13:32:23 +0900

    Apply code style

--- app/src/main/java/net/nessness/android/thkinoko/MainActivityFragment.java
+++ app/src/main/java/net/nessness/android/thkinoko/MainActivityFragment.java
@@ -1,7 +1,7 @@
 package net.nessness.android.thkinoko;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +11,11 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
-    public MainActivityFragment() {
-    }
+  public MainActivityFragment() {
+  }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
-    }
+  @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_main, container, false);
+  }
 }
